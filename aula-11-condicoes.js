b01a5fd98921c5ea5cf8a3c152f66f74f9484a59
unknown <fois2010@gmail.com>
2019-08-22 14:32:56 -0300

    Aula-12

--- aula-11-condicoes.js
+++ aula-11-condicoes.js
@@ -1,1 +1,8 @@
-console.log('O console funcionou correctamente')
+var vel = 12
+
+console.log(`A sua velocidade do seu carro é igual a ${vel}`)
+
+if (vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre usando o cinto de segurança')
