4ab5bb5c9aa02366b07e45c6cd257a61979538b3
unknown <fois2010@gmail.com>
2019-08-22 21:54:36 -0300

    Aula-14 Repetiçoes

---
+++ aula-13-repeticoes-while/aula-13.1-repeticoes.js
@@ -0,0 +1,16 @@
+var c = 0
+do {
+    console.log(`Passo ${c}`)
+    c++
+
+ } while ( c <= 10 )
+
+
+
+/*
+var c = 0
+while ( c <= 10 ){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
---
+++ aula-14-repeticoes-for/aula-14-repeticoes-for.js
@@ -0,0 +1,11 @@
+
+console.log('Vai començar...')
+
+
+for (c = 1; c <= 5; c ++){
+
+    console.log(c)
+
+}
+
+console.log('FIM!')
