a71f44eb87e4f656e9c95ed84dcdc98b1c7b29f9
unknown <fois2010@gmail.com>
2019-08-23 16:47:19 -0300

    Aula-15 Arrays-Vetores

---
+++ Aula-15-Array-Vetores/aula-15-Array-Vetores na tela.js
@@ -0,0 +1,13 @@
+
+let valores = [5,8,2,4,1]
+
+
+//for (let pos=0; pos < valores.length; pos++){
+  //  console.log(`Posição ${pos}, Valor ${valores[pos]}`)
+//}
+
+for (let pos in valores){
+    console.log(`posiçao ${pos}, Valores ${valores[pos]}`)
+}
+
+console.log(valores.indexOf(1))
---
+++ Aula-15-Array-Vetores/aula-15-Array-Vetores.js
@@ -0,0 +1,8 @@
+
+
+let num = [5,8,2,4,1]
+num.push(44, 3)
+num.sort()
+console.log(num)
+console.log(num.length)
+console.log(num[0])
